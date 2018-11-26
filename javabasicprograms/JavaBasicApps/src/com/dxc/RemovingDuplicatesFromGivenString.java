cc6d3fbe4620edbcec42fdeda4a5b0dee26e6233
mrao48 <mrao48@INCNU1492MSS.CSCMWS.CSCMWS.COM>
2018-11-26 12:37:56 +0530

    String Example

---
+++ javabasicprograms/JavaBasicApps/src/com/dxc/RemovingDuplicatesFromGivenString.java
@@ -0,0 +1,14 @@
+package com.dxc;
+
+import java.util.Scanner;
+
+public class RemovingDuplicatesFromGivenString {
+	
+	public static void main(String... ar){
+		
+		Scanner scr = new Scanner(System.in);
+		System.out.println("Enter the Input String : ");
+		scr.close();
+	}
+
+}
