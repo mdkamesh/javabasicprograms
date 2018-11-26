3b9242d1758fff17e375e6fd4679b298378f7c67
mrao48 <mrao48@INCNU1492MSS.CSCMWS.CSCMWS.COM>
2018-11-26 14:18:31 +0530

    String Example

--- javabasicprograms/JavaBasicApps/src/com/dxc/RemovingDuplicatesFromGivenString.java
+++ javabasicprograms/JavaBasicApps/src/com/dxc/RemovingDuplicatesFromGivenString.java
@@ -1,14 +1,32 @@
 package com.dxc;
 
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class RemovingDuplicatesFromGivenString {
 	
 	public static void main(String... ar){
 		
+		
 		Scanner scr = new Scanner(System.in);
 		System.out.println("Enter the Input String : ");
+		String originalData = scr.nextLine();
+		
+		char[] chars = originalData.toCharArray();
+		Set<Character> charSet = new LinkedHashSet<Character>();
+		for (char c : chars) {
+		    charSet.add(c);
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (Character character : charSet) {
+		    sb.append(character);
+		}
+		System.out.println(sb.toString());
+		
 		scr.close();
+		
 	}
 
 }
