1fd019ab27e9b4353ec2aeb2b56fcad3dbd7d7f9
mrao48 <mrao48@INCNU1492MSS.CSCMWS.CSCMWS.COM>
2018-11-26 14:39:34 +0530

    ReverseOfaString

---
+++ javabasicprograms/JavaBasicApps/src/com/dxc/ReverseOfaString.java
@@ -0,0 +1,19 @@
+package com.dxc;
+
+import java.util.Scanner;
+
+public class ReverseOfaString {
+	
+	public static void main(String... ar){
+		
+		// Method 1 Using StringBuffer()
+		
+		Scanner scr = new Scanner(System.in);
+		System.out.println("Enter the Input String : ");
+		String inputData = scr.nextLine();
+		System.out.println(new StringBuffer(inputData).reverse());
+		
+		
+	}
+
+}
